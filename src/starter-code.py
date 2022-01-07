168ccc7e84cc7b7408a51ceb66ea974d4630caf6
alexmcclung <mcclungalex@gmail.com>
2022-01-07 16:49:01 -0500

    updating code

--- src/starter-code.py
+++ src/starter-code.py
@@ -1,14 +1,23 @@
 # -*- coding: utf-8 -*-
-"""
-Load Times Rankings Data
-
-"""
 
 import pandas as pd
+import plotly.express as px
 
 url = 'https://github.com/AlexMcClung/World-University-Rankings/raw/master/data/timesData.csv'
 
 df = pd.read_csv(url, engine = 'python')
 
 df['year'].value_counts()
+
+df.query("year == 2016").head()
+
+df['Rank'] = df.world_rank.str.extract('(\d+)').astype(int)
+
+hpy = df[df['university_name'].isin(['Yale University','Princeton University', 'Harvard University'])]
+
+fig = px.line(hpy, x = "year", y = "Rank", color='university_name')
+
+fig.update_yaxes(autorange="reversed")
+
+fig.show()
 
